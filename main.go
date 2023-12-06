aac6b661ed5ffc39365e1886f4fe3ad07ecd1de2
Erik Falk <erik.falk@oerlikon.com>
2023-12-06 13:58:00 +0100

    added remove function

--- main.go
+++ main.go
@@ -5,19 +5,20 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path"
+	"slices"
+	"strconv"
 	"time"
 )
 
 type Todo struct {
-	Id int
+	Id   int
 	Task string
 	Done bool
 }
 
-var todos = map[string][]Todo{
-	"Todos": {
-		
-	},}
+var data = map[string][]Todo{
+	"Todos": {}}
 
 var createdTodos = 0
 
@@ -26,21 +27,31 @@ func main() {
 
 	http.HandleFunc("/", listTodos)
 	http.HandleFunc("/add-todo/", addTodo)
+	http.HandleFunc("/remove-todo/", removeTodo)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func listTodos(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("index.html"))
-	tmpl.Execute(w, todos)
+	tmpl.Execute(w, data)
 }
 
 func addTodo(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(1 * time.Second)
 	task := r.PostFormValue("task")
 
-	newTodo :=Todo{Id: createdTodos+1, Task: task, Done: false}
+	createdTodos = createdTodos +1
+	newTodo := Todo{Id: createdTodos, Task: task, Done: false}
+	
+	data["Todos"] = append(data["Todos"], newTodo)
 
 	tmpl := template.Must(template.ParseFiles("index.html"))
 	tmpl.ExecuteTemplate(w, "todo-list-element", newTodo)
+}
+
+func removeTodo(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(path.Base(r.URL.Path))
+
+	data["Todos"] = slices.DeleteFunc(data["Todos"], func(t Todo) bool { return t.Id == id })
 }
