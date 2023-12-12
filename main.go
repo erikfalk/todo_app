93d75de8ba5b8e368fd87267ddce522a866345d0
Erik Falk <erik.falk@oerlikon.com>
2023-12-12 08:34:12 +0100

    added todo update function

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -23,11 +22,12 @@ var data = map[string][]Todo{
 var createdTodos = 0
 
 func main() {
-	fmt.Println("Server started and serving on port 8000.")
+	log.Print("Server started and serving on port 8000.")
 
 	http.HandleFunc("/", listTodos)
 	http.HandleFunc("/add-todo/", addTodo)
 	http.HandleFunc("/remove-todo/", removeTodo)
+	http.HandleFunc("/update-todo/", updateTodo)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
@@ -41,17 +41,32 @@ func addTodo(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(1 * time.Second)
 	task := r.PostFormValue("task")
 
-	createdTodos = createdTodos +1
+	createdTodos = createdTodos + 1
 	newTodo := Todo{Id: createdTodos, Task: task, Done: false}
-	
+
 	data["Todos"] = append(data["Todos"], newTodo)
 
 	tmpl := template.Must(template.ParseFiles("index.html"))
 	tmpl.ExecuteTemplate(w, "todo-list-element", newTodo)
+	log.Print("Added new todo: ", newTodo)
 }
 
 func removeTodo(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(path.Base(r.URL.Path))
 
 	data["Todos"] = slices.DeleteFunc(data["Todos"], func(t Todo) bool { return t.Id == id })
+}
+
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(path.Base(r.URL.Path))
+
+	var todo *Todo =  &data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
+
+	if todo.Done {
+		todo.Done = false
+	} else {
+		todo.Done = true
+	}
+
+	log.Print(data["Todos"])
 }
