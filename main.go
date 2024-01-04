4b069f2cab734c90feed94532b7141c59b7de0cc
Erik Falk <erik-falk@web.de>
2024-01-04 18:12:33 +0100

    moved todo functions into own package

--- main.go
+++ main.go
@@ -1,107 +1,22 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
-	"path"
-	"slices"
-	"strconv"
+
+	"github.com/erikfalk/todo_app/todo"
 )
-
-type Todo struct {
-	Id   int
-	Task string
-	Done bool
-}
-
-var data = map[string][]Todo{
-	"Todos": {}}
-
-var createdTodos = 0
 
 func main() {
 	log.Print("Server started and serving on port 8000.")
 
-	http.HandleFunc("/", listTodos)
-	http.HandleFunc("/add-todo/", addTodo)
-	http.HandleFunc("/remove-todo/", removeTodo)
-	http.HandleFunc("/set-status/", setStatus)
-	http.HandleFunc("/start-update/", startUpdate)
-	http.HandleFunc("/save-update/", saveUpdate)
-	http.HandleFunc("/cancel-update/", cancelUpdate)
+	http.HandleFunc("/", todo.ListTodos)
+	http.HandleFunc("/add-todo/", todo.AddTodo)
+	http.HandleFunc("/remove-todo/", todo.RemoveTodo)
+	http.HandleFunc("/set-status/", todo.SetStatus)
+	http.HandleFunc("/start-update/", todo.StartUpdate)
+	http.HandleFunc("/save-update/", todo.SaveUpdate)
+	http.HandleFunc("/cancel-update/", todo.CancelUpdate)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
-}
-
-func saveUpdate(w http.ResponseWriter, r *http.Request) {
-	task := r.PostFormValue("task")
-	id, _ := strconv.Atoi(path.Base(r.URL.Path))
-
-	var todo *Todo = &data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
-
-	todo.Task = task
-
-	tmpl := template.Must(template.ParseFiles("templates/task_view_tmpl.html"))
-	tmpl.ExecuteTemplate(w, "todo-list-row", todo)
-
-	log.Print("Todo updated: ", todo)
-}
-
-func cancelUpdate(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(path.Base(r.URL.Path))
-
-	var todo Todo = data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
-	tmpl := template.Must(template.ParseFiles("templates/task_view_tmpl.html"))
-	tmpl.ExecuteTemplate(w, "todo-list-row", todo)
-}
-
-func startUpdate(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(path.Base(r.URL.Path))
-
-	var todo Todo = data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
-	tmpl := template.Must(template.ParseFiles("templates/task_input_tmpl.html"))
-	tmpl.ExecuteTemplate(w, "todo-list-row", todo)
-}
-
-func listTodos(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("index.html"))
-	tmpl.Execute(w, data)
-}
-
-func addTodo(w http.ResponseWriter, r *http.Request) {
-	// for spinner testing purposes
-	//time.Sleep(1 * time.Second)
-	task := r.PostFormValue("task")
-
-	createdTodos = createdTodos + 1
-	newTodo := Todo{Id: createdTodos, Task: task, Done: false}
-
-	data["Todos"] = append(data["Todos"], newTodo)
-
-	tmpl := template.Must(template.ParseFiles("templates/task_view_tmpl.html"))
-	tmpl.ExecuteTemplate(w, "todo-list-row", newTodo)
-	log.Print("Created new todo: ", newTodo)
-}
-
-func removeTodo(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(path.Base(r.URL.Path))
-
-	data["Todos"] = slices.DeleteFunc(data["Todos"], func(t Todo) bool { return t.Id == id })
-
-	log.Print("Deleted todo with id: ", id)
-}
-
-func setStatus(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.Atoi(path.Base(r.URL.Path))
-
-	var todo *Todo = &data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
-
-	if todo.Done {
-		todo.Done = false
-	} else {
-		todo.Done = true
-	}
-
-	log.Print("Updated Todo: ", *todo)
 }
---
+++ todo/todo.go
@@ -0,0 +1,93 @@
+package todo
+
+import (
+	"log"
+	"net/http"
+	"path"
+	"slices"
+	"strconv"
+	"text/template"
+)
+
+type Todo struct {
+	Id   int
+	Task string
+	Done bool
+}
+
+var data = map[string][]Todo{
+	"Todos": {}}
+
+var createdTodos = 0
+
+func SaveUpdate(w http.ResponseWriter, r *http.Request) {
+	task := r.PostFormValue("task")
+	id, _ := strconv.Atoi(path.Base(r.URL.Path))
+
+	var todo *Todo = &data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
+
+	todo.Task = task
+
+	tmpl := template.Must(template.ParseFiles("templates/task_view_tmpl.html"))
+	tmpl.ExecuteTemplate(w, "todo-list-row", todo)
+
+	log.Print("Todo updated: ", todo)
+}
+
+func CancelUpdate(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(path.Base(r.URL.Path))
+
+	var todo Todo = data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
+	tmpl := template.Must(template.ParseFiles("templates/task_view_tmpl.html"))
+	tmpl.ExecuteTemplate(w, "todo-list-row", todo)
+}
+
+func StartUpdate(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(path.Base(r.URL.Path))
+
+	var todo Todo = data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
+	tmpl := template.Must(template.ParseFiles("templates/task_input_tmpl.html"))
+	tmpl.ExecuteTemplate(w, "todo-list-row", todo)
+}
+
+func ListTodos(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, data)
+}
+
+func AddTodo(w http.ResponseWriter, r *http.Request) {
+	// for spinner testing purposes
+	//time.Sleep(1 * time.Second)
+	task := r.PostFormValue("task")
+
+	createdTodos = createdTodos + 1
+	newTodo := Todo{Id: createdTodos, Task: task, Done: false}
+
+	data["Todos"] = append(data["Todos"], newTodo)
+
+	tmpl := template.Must(template.ParseFiles("templates/task_view_tmpl.html"))
+	tmpl.ExecuteTemplate(w, "todo-list-row", newTodo)
+	log.Print("Created new todo: ", newTodo)
+}
+
+func RemoveTodo(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(path.Base(r.URL.Path))
+
+	data["Todos"] = slices.DeleteFunc(data["Todos"], func(t Todo) bool { return t.Id == id })
+
+	log.Print("Deleted todo with id: ", id)
+}
+
+func SetStatus(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(path.Base(r.URL.Path))
+
+	var todo *Todo = &data["Todos"][slices.IndexFunc(data["Todos"], func(t Todo) bool { return t.Id == id })]
+
+	if todo.Done {
+		todo.Done = false
+	} else {
+		todo.Done = true
+	}
+
+	log.Print("Updated Todo: ", *todo)
+}
